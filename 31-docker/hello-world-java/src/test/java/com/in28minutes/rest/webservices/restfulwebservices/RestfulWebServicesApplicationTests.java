f8342e1c0bcf1ff2c68b647166165b030f0b87c0
Ranga Karanam <ranga@Rangas-MacBook-Pro-2.local>
2022-11-03 18:31:35 +0530

    Thank You for Choosing to Learn from in28Minutes

---
+++ 31-docker/hello-world-java/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestfulWebServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
