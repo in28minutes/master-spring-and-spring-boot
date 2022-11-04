f833e2dc8f9d0933b8a259cdc83948cff8bda5b7
Ranga Karanam <in28minutes@gmail.com>
2022-11-04 10:24:10 +0530

    Thank You for Choosing to Learn from in28Minutes

--- 31-docker/hello-world-java/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ 31-docker/hello-world-java/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/")
 	public String helloWorld() {
-		return "{\"message\":\"Hello World Java v5\"}";
+		return "{\"message\":\"Hello World Java v1\"}";
 	}
 }
