584ad890a19b46ea9b7320aab06cb5eff7b24b8a
Ranga Karanam <ranga@Rangas-MacBook-Pro-2.local>
2022-11-07 16:54:14 +0530

    Thank You for Choosing to Learn from in28Minutes

---
+++ 41-full-stack/01-frontend/todo-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LearningComponent from './components/learning-examples/LearningComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <LearningComponent />
+    </div>
+  )
+}
+
+export default App;
