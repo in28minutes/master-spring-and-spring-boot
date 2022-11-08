71ad04c42f779d838bc198fd583cb1a7bb0114f3
Ranga Karanam <ranga@Rangas-MacBook-Pro-2.local>
2022-11-08 13:53:17 +0530

    Thank You for Choosing to Learn from in28Minutes

--- 41-full-stack/01-frontend/todo-app/src/components/todo/TodoApp.jsx
+++ 41-full-stack/01-frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react'
-import {BrowserRouter, Routes, Route, useNavigate, useParams} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
     return (
         <div className="TodoApp">
             <BrowserRouter>
+                <HeaderComponent />
                 <Routes>
-                    <Route path='/' element={ <LoginComponent /> }></Route>
-                    <Route path='/login' element={ <LoginComponent /> }></Route>
-                    <Route path='/welcome/:username' element={<WelcomeComponent /> }></Route>
-                    <Route path='*' element={<ErrorComponent /> }></Route>
+                    <Route path='/' element={ <LoginComponent /> } />
+                    <Route path='/login' element={ <LoginComponent /> } />
+                    <Route path='/welcome/:username' element={<WelcomeComponent /> } />
+                    <Route path='/todos' element={<ListTodosComponent /> } />
+                    <Route path='/logout' element={<LogoutComponent /> } />
+                    
+                    <Route path='*' element={<ErrorComponent /> } />
+
                 </Routes>
+                <FooterComponent />
             </BrowserRouter>
-            
-            
         </div>
     )
 }
@@ -85,7 +89,7 @@ function WelcomeComponent() {
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
             <div>
-                Welcome Component
+                Manage your todos - <Link to="/todos">Go here</Link>
             </div>
         </div>
     )
@@ -97,6 +101,99 @@ function ErrorComponent() {
             <h1>We are working really hard!</h1>
             <div>
                 Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.
+            </div>
+        </div>
+    )
+}
+
+function ListTodosComponent() {
+
+    const today = new Date()
+    
+    const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
+    
+    const todos = [
+                    {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
+                    {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
+                    {id: 3, description: 'Learn DevOps', done: false, targetDate:targetDate},
+                ]
+
+
+    return (
+        <div className="container">
+            <h1>Things You Want To Do!</h1>
+            <div>
+                <table className="table">
+                    <thead>
+                            <tr>
+                                <td>ID</td>
+                                <td>Description</td>
+                                <td>Is Done?</td>
+                                <td>Target Date</td>
+                            </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        todos.map(
+                            todo => (
+                                <tr key={todo.id}>
+                                    <td>{todo.id}</td>
+                                    <td>{todo.description}</td>
+                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.targetDate.toDateString()}</td>
+                                </tr>
+                            )
+                        )
+                    }
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    )
+}
+
+function HeaderComponent() {
+    return (
+        
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+            <div className="container">
+                <div className="row">
+                    <nav className="navbar navbar-expand-lg">
+                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.in28minutes.com">in28minutes</a>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav">
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                            </ul>
+                        </div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+function FooterComponent() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                Your Footer
+            </div>
+        </footer>
+    )
+}
+
+function LogoutComponent() {
+    return (
+        <div className="LogoutComponent">
+            <h1>You are logged out!</h1>
+            <div>
+                Thank you for using our App. Come back soon!
             </div>
         </div>
     )
--- 41-full-stack/01-frontend/todo-app/src/index.js
+++ 41-full-stack/01-frontend/todo-app/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
