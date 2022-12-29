b2ae9506a1386cf492e1c6342eec8a4043f4b9c0
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2022-12-29 17:01:42 +0300

    learn-spring-framework web app. is added.

---
+++ 01-spring/01-Step03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+	}
+
+}
