0a9cb2e09c7633b4ecbe1ac371c00dc2c079b626
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2022-12-31 23:44:20 +0300

    Update App02HelloWorldSpring.java

--- 01-spring/01-Step03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ 01-spring/01-Step03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -17,8 +17,10 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
         System.out.println(context.getBean("address2"));
-        System.out.println(context.getBean(Address.class));
+        //System.out.println(context.getBean(Address.class));
     }
 
 }
