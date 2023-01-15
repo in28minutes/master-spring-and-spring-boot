6493bc2e091dab124cb7af98d9dc032a5bfc759b
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-01-15 15:27:22 +0300

    GamingConfiguration class is removed.

--- 01-spring/01-Step03/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ 01-spring/01-Step03/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -2,13 +2,28 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class App03GamingSpringBeans {
+
+    @Bean
+    public GamingConsole game() {
+        GamingConsole game = new PacManGame();
+        return game;
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        GameRunner gameRunner = new GameRunner(game);
+        return gameRunner;
+    }
 
     public static void main(String[] args) {
         try (AnnotationConfigApplicationContext context =
                       new AnnotationConfigApplicationContext
-                              (GamingConfiguration.class)) {
+                              (App03GamingSpringBeans.class)) {
 
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
