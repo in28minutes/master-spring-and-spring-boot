e55ce69091cbbea1b6bb767e343cff5a7f1f1e1a
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-01-15 18:02:21 +0300

    primary annotation

--- 01-spring/01-Step03/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
+++ 01-spring/01-Step03/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
@@ -1,5 +1,8 @@
 package com.in28minutes.learnspringframework.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MarioGame implements GamingConsole {
 
     public void up() {
--- 01-spring/01-Step03/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/PacManGame.java
+++ 01-spring/01-Step03/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/PacManGame.java
@@ -1,8 +1,10 @@
 package com.in28minutes.learnspringframework.game;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class PacManGame implements GamingConsole {
 
     @Override
