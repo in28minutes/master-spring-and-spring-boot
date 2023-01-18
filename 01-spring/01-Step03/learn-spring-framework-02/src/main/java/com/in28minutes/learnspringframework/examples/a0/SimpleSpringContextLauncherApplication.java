31624fccdb6d09be99484a568b68df645b6c28ac
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-01-18 05:07:24 +0300

    Update SimpleSpringContextLauncherApplication.java

--- 01-spring/01-Step03/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a0/SimpleSpringContextLauncherApplication.java
+++ 01-spring/01-Step03/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a0/SimpleSpringContextLauncherApplication.java
@@ -1,5 +1,6 @@
 package com.in28minutes.learnspringframework.examples.a0;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +10,29 @@ import java.util.Arrays;
 
 @Component
 class YourBusinessClass {
+    //@Autowired
     Dependency1 dependency1;
+    //@Autowired
     Dependency2 dependency2;
+
+    //@Autowired
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        System.out.println("Constructor Injection - YourBusinessClass");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
+    //    @Autowired
+//    public void setDependency1(Dependency1 dependency1) {
+//        System.out.println("Setter Injection - setDependency1");
+//        this.dependency1 = dependency1;
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//        System.out.println("Setter Injection - setDependency2");
+//        this.dependency2 = dependency2;
+//    }
 
     /*public String toString() {
         return "Using" + dependency1 + " and " + dependency2;
@@ -19,7 +41,7 @@ class YourBusinessClass {
     // more efficient way for concatenation via StringBuffer
     public String toString() {
         return new StringBuffer()
-                .append("Using")
+                .append("Using ")
                 .append(dependency1)
                 .append(" and ")
                 .append(dependency2).toString();
