3897ab115ab3e3301bc61f7a0babd66096e394d0
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-17 12:34:43 +0300

    Update XmlConfigurationContextLauncherApplication.java

--- 01-spring/01-Step03/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
+++ 01-spring/01-Step03/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/h1/XmlConfigurationContextLauncherApplication.java
@@ -1,23 +1,20 @@
 package com.in28minutes.learnspringframework.examples.h1;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import com.in28minutes.learnspringframework.game.GameRunner;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
 
-@Configuration
-@ComponentScan
 public class XmlConfigurationContextLauncherApplication {
 
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext context =
-                      new AnnotationConfigApplicationContext
-                              (XmlConfigurationContextLauncherApplication.class)) {
+        try (ClassPathXmlApplicationContext context =
+                     new ClassPathXmlApplicationContext("contextConfiguration.xml")) {
             Arrays.stream(context.getBeanDefinitionNames())
                     .forEach(System.out::println);
-                    
+            System.out.println(context.getBean("name"));
+            System.out.println(context.getBean("age"));
+            context.getBean(GameRunner.class).run();
         }
     }
 }
