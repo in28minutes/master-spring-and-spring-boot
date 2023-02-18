1d56feebef0093c2c9bb6a01fa0a7d4c357a013a
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-18 09:05:34 +0300

    Create CurrencyServiceConfiguration.java

---
+++ 02-springboot/learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CurrencyServiceConfiguration.java
@@ -0,0 +1,37 @@
+package com.in28minutes.springboot.learnspringboot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "currency-service")
+@Component
+public class CurrencyServiceConfiguration {
+
+    private String url;
+    private String username;
+    private String key;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
