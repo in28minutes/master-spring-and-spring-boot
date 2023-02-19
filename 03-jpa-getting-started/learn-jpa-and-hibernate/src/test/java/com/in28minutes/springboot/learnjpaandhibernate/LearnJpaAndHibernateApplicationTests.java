b31af5d49cc545d79858b62659ae55957be93e9b
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-19 10:26:28 +0300

    initialize project

---
+++ 03-jpa-getting-started/learn-jpa-and-hibernate/src/test/java/com/in28minutes/springboot/learnjpaandhibernate/LearnJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learnjpaandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
