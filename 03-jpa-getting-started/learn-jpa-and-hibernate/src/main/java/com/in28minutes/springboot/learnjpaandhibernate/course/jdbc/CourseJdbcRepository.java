2382c35e6f89e88830b9d43f45ee839d707895d6
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-19 21:34:12 +0300

    Create CourseJdbcRepository.java

---
+++ 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,23 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+
+    private static String INSERT_QUERY =
+            """
+                insert into course (id, name, author)
+                values(1, 'Learn AWS','in28minutes');
+            """;
+
+    public void insert() {
+        springJdbcTemplate.update(INSERT_QUERY);
+    }
+
+}
