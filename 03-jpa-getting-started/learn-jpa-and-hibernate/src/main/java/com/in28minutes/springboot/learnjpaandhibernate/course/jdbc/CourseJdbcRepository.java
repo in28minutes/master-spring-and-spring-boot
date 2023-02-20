6b794cec3b4f07ef0b3a8ae36dac2c13ddc9d158
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-20 22:02:23 +0300

    Update CourseJdbcRepository.java

--- 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -13,11 +14,22 @@ public class CourseJdbcRepository {
     private static String INSERT_QUERY =
             """
                 insert into course (id, name, author)
-                values(1, 'Learn AWS','in28minutes');
+                values(?, ?,  ?);
             """;
 
-    public void insert() {
-        springJdbcTemplate.update(INSERT_QUERY);
+    private static String DELETE_QUERY =
+            """
+                delete from course
+                where id = ?;
+            """;
+
+    public void insert(Course course) {
+        springJdbcTemplate.update(INSERT_QUERY,
+                course.getId(), course.getName(), course.getAuthor());
+    }
+
+    public void deleteById(long id) {
+        springJdbcTemplate.update(DELETE_QUERY, id);
     }
 
 }
