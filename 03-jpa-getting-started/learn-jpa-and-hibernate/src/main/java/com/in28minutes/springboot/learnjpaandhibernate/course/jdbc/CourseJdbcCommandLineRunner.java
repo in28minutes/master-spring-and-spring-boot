95105e805c2a605ca1f7287090528eaceb70b0b3
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-20 22:02:21 +0300

    Update CourseJdbcCommandLineRunner.java

--- 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,10 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, "Learn AWS now", "in28minutes"));
+        repository.insert(new Course(2, "Learn Azure now", "in28minutes"));
+        repository.insert(new Course(3, "Learn DevOps now", "in28minutes"));
+
+        repository.deleteById(1);
     }
 }
