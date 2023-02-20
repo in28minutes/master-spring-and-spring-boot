899409abeea51f4fbdf8ef469364ea770792d908
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-20 22:02:19 +0300

    Create Course.java

---
+++ 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/Course.java
@@ -0,0 +1,39 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course;
+
+public class Course {
+
+    private long id;
+    private String name;
+    private String author;
+
+    public Course() {
+
+    }
+
+    public Course(long id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
