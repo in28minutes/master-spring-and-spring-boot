7f84eb7203b7b2d10761d9d6ba9fad2684aadcfc
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-21 13:11:20 +0300

    Create CourseJpaRepository.java

---
+++ 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jpa/CourseJpaRepository.java
@@ -0,0 +1,29 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course.jpa;
+
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public class CourseJpaRepository {
+    //@Autowired
+    @PersistenceContext // more specific annotation
+    private EntityManager entityManager;
+
+    public void insert(Course course) {
+        entityManager.merge(course);
+    }
+
+    public Course findById(long id) {
+        return entityManager.find(Course.class, id);
+    }
+
+    public void deleteById(long id) {
+        Course course = entityManager.find(Course.class, id);
+        entityManager.remove(course);
+    }
+}
