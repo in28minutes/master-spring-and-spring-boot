eb972ec95b8873ff1525dd23a6cb3d4d5e132e9f
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-21 14:07:12 +0300

    Update CourseSpringDataJpaRepository.java

--- 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
+++ 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -3,5 +3,9 @@ package com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa;
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+    List<Course> findByAuthor(String author);
+    List<Course> findByName(String name);
 }
