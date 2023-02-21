35b301ce14923d7ba411c5906ca2bd8dded8b68b
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-21 13:50:50 +0300

    Update CourseCommandLineRunner.java

--- 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.learnjpaandhibernate.course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,18 +14,21 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepository repository;
 
+//    @Autowired
+//    private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS Jpa!", "in28minutes"));
-        repository.insert(new Course(2, "Learn Azure Jpa!", "in28minutes"));
-        repository.insert(new Course(3, "Learn DevOps Jpa!", "in28minutes"));
+        repository.save(new Course(1, "Learn AWS Jpa!", "in28minutes"));
+        repository.save(new Course(2, "Learn Azure Jpa!", "in28minutes"));
+        repository.save(new Course(3, "Learn DevOps Jpa!", "in28minutes"));
 
-        repository.deleteById(1);
+        repository.deleteById(1l);
 
-        System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        System.out.println(repository.findById(2l));
+        System.out.println(repository.findById(3l));
     }
 }
