69e530a883ba5f41c611d392a7921e3a2a885fc1
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-24 15:51:09 +0300

    Update LoginController.java

--- 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -5,14 +5,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("login")
+    @RequestMapping(value="login",method = RequestMethod.GET)
     public String gotoLoginPage() {
         return "login";
+    }
+
+    @RequestMapping(value="login",method = RequestMethod.POST)
+    public String gotoWelcomePage(@RequestParam String name,
+                                  @RequestParam String password, ModelMap model) {
+        model.put("name", name);
+        model.put("password", password);
+
+        return "welcome";
     }
 
 }
