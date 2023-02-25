4b3c7bb6e4d0ee27e353c7075d8e0a36ade3cdd1
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-25 22:36:18 +0300

    Create TodoController.java

---
+++ 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -0,0 +1,26 @@
+package com.in28minutes.springboot.myfirstwebapp.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @RequestMapping("list-todos")
+    public String listAllTodos(ModelMap model) {
+        List<Todo> todos = todoService.findByUsername("in28minutes");
+        model.addAttribute("todos", todos);
+
+        return "listTodos";
+    }
+
+}
