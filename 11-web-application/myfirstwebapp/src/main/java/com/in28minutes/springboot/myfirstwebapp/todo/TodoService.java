20b6d34cec9e3803cb52ccc97c70422cc7ba301c
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-02-25 22:20:50 +0300

    Create TodoService.java

---
+++ 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,21 @@
+package com.in28minutes.springboot.myfirstwebapp.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoService {
+    private static List<Todo> todos = new ArrayList<>();
+    static {
+        todos.add(new Todo(1, "in28minutes", "Learn AWS",
+                LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(2, "in28minutes", "Learn DevOps",
+                LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(3, "in28minutes", "Learn Full Stack Development",
+                LocalDate.now().plusYears(3), false));
+    }
+
+    public List<Todo> findByUsername(String username) {
+        return todos;
+    }
+}
