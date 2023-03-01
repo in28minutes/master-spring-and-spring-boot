a4adcc204957ad2d94a3eaa14af33911a058ec19
Abc Def Mac (SourceTree) <69718370+javasamplecodes@users.noreply.github.com>
2023-03-01 17:44:06 +0300

    Update TodoService.java

--- 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -9,16 +9,23 @@ import java.util.List;
 @Service
 public class TodoService {
     private static List<Todo> todos = new ArrayList<>();
+    private static int todosCount = 0;
+
     static {
-        todos.add(new Todo(1, "in28minutes", "Learn AWS",
+        todos.add(new Todo(++todosCount, "in28minutes", "Learn AWS",
                 LocalDate.now().plusYears(1), false));
-        todos.add(new Todo(2, "in28minutes", "Learn DevOps",
+        todos.add(new Todo(++todosCount, "in28minutes", "Learn DevOps",
                 LocalDate.now().plusYears(2), false));
-        todos.add(new Todo(3, "in28minutes", "Learn Full Stack Development",
+        todos.add(new Todo(++todosCount, "in28minutes", "Learn Full Stack Development",
                 LocalDate.now().plusYears(3), false));
     }
 
     public List<Todo> findByUsername(String username) {
         return todos;
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+        Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+        todos.add(todo);
     }
 }
