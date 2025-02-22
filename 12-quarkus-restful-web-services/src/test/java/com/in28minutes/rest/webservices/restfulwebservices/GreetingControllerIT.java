4b24d796ac24ef835c4159da4909800b0635003a
bsmahi <bs.mahi@gmail.com>
2025-02-22 17:12:53 +0530

    Migrated Java RestApi to Quarkus

--- 12-quarkus-restful-web-services/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/GreetingControllerIT.java
+++
@@ -1,8 +0,0 @@
-package com.in28minutes.rest.webservices.restfulwebservices;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingControllerIT extends GreetingControllerTest {
-    // Execute the same tests but in packaged mode.
-}
